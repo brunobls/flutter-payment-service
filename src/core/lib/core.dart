d32ae3bcf01dd7808423a865de6a932b11b05215
Bruno Lima <brunoblsouza@icloud.com>
2022-01-30 17:51:53 -0300

    change core project

--- src/core/lib/core.dart
+++ src/core/lib/core.dart
@@ -1,7 +1,61 @@
 library core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+typedef WidgetBuilderArgs = Widget Function(BuildContext context, Object? args);
+late GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+abstract class BuilderBase {
+  List<Module> get modules;
+
+  Map<String, WidgetBuilderArgs> get builderRoutes;
+
+  final Map<String, WidgetBuilderArgs> routes = {};
+
+  void registerRouters() {
+    if (builderRoutes.isNotEmpty) routes.addAll(builderRoutes);
+    if (modules.isNotEmpty) {
+      for (Module module in modules) {
+        routes.addAll(module.routes);
+      }
+    }
+  }
+
+  void registerInjections() {
+    if (modules.isNotEmpty) {
+      for (Module module in modules) {
+        module.registerInjections();
+      }
+    }
+  }
+
+  void registerListeners() {
+    if (modules.isNotEmpty) {
+      for (Module module in modules) {
+        module.registerListeners();
+      }
+    }
+  }
+
+  Route<dynamic>? generateRoute(RouteSettings settings) {
+    var routerName = settings.name;
+    var routerArgs = settings.arguments;
+
+    var navigateTo = routes[routerName];
+    if (navigateTo == null) return null;
+
+    return MaterialPageRoute(
+      builder: (context) => navigateTo.call(context, routerArgs),
+    );
+  }
+}
+
+abstract class Module {
+  String get moduleName;
+
+  Map<String, WidgetBuilderArgs> get routes;
+
+  void Function() get registerInjections;
+
+  void Function() get registerListeners;
 }
